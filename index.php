1ee28d89e564e81c8ee8331c0e1a2382a23f3918
GilangDev-Web <158812882+GilangDev-Web@users.noreply.github.com>
2024-03-10 03:20:05 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="atasan">
+      <h1 class="judul">LANGZZ STORE BIO</h1>
+      <p class="deskripsi">CARA GAMPANG AKSES MEDIA KITA</p>
+    </div>
+    
+    <div id="container-link">
+        <div id="link-item-wa">
+            <a href="https://wa.me/62857223422150">WhatsApp</a>
+        </div>
+        <div id="link-item-ig">
+            <a href="http://instagram.com/langz_store.real">Instagram</a>
+        </div>
+        <div id="link-item-tt">
+            <a href="tiktok.php">Tiktok</a>
+        </div>
+        <div id="link-item-storeweb">
+            <a href="https://glgw.my.id">Langz Store Web</a>
+        </div>
+    </div>
+</body>
+</html>
